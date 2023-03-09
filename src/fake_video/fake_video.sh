f525f81b991bf72895ef759f9906d75b90c6b9b5
coccocarmiano <matteo.quarta.carmiano@gmail.com>
2023-03-09 21:10:38 +0100

    video creation

--- src/fake_video/fake_video.sh
+++ src/fake_video/fake_video.sh
@@ -1,7 +1,7 @@
 modprobe v4l2loopback devices=1 video_nr=1 card_label='MGI'
 n=$(ls /dev/video* | sort -nr | head -n1 | grep -Eo "[0-9]")
 ffmpeg -stream_loop -1 \
-       -re -i "/home/cocco/Scrivania/cbcr.mp4" \
+       -re -i "data/mauri/videos/labeled.mp4" \
        -vcodec rawvideo \
        -threads 0 \
        -f v4l2 \
